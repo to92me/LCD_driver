e34e07823672f8700d37ca503df6eceb92668c0f
to92me <to92me@gmail.com>
2015-01-21 13:44:46 +0100

    finished app

--- app/displ.c
+++ app/displ.c
@@ -1,6 +1,5 @@
 /*
  *  ioctl.c - the process to use ioctl's to control the kernel module
-
  */
 
 //#include "ioctl-definicije.h"// isti h fajl mora biti i u device kodu i u program kodu
@@ -15,13 +14,13 @@
 //#include "myPic.h" // slika predstavljena kao jednodimenzionalni niz
 #define DEVICE "/dev/displej"
 #define  NAREDBE_BUFFER   6
-//#define LPC -- use for home compiling and testing without LPC borad
-
+#define LPC
+#undef LPC // undef for home compiling and testing without LPC borad
 int index_tmp;
 char* next;
 int index_count;
 char *login[50];
-int login_int[15];
+unsigned int login_int[15];
 int tmp;
 
 int file_desc;
@@ -35,20 +34,21 @@ int main( int argc, char **argv){
 
     int next_option, option_count;
     char* optarg_tmp;
-    const char* const short_options = "f:s:p:hl:k:";
+    const char* const short_options = "s:p:hl:k:b:c:";
 
     const struct option long_options[] = {
-    {"function", required_argument, NULL, 'f'},
     {"sentence", required_argument, NULL, 's'},
     {"picture", no_argument, NULL, 'p'},
     {"help", no_argument, NULL, 'h'},
     {"plotline", required_argument, NULL, 'l'},
     {"plotsquare", required_argument, NULL, 'k'},
+    {"color", required_argument, NULL, 'b'},
+    {"circle", required_argument, NULL, 'c'},
     {NULL, 0, NULL , 0}
 };
 
 #ifdef LPC
-    file_desc = open(DEVICE, 0); //pokrecemo drajver
+    file_desc = open(DEVICE, 0);
     if (file_desc < 0) {
         printf("Can't open device file: %s\n", DEVICE);
         exit(-1);
@@ -66,20 +66,12 @@ int main( int argc, char **argv){
     }
 
     switch (next_option) {
-    case 'f':{
-        printf("usao u f\n");
-        tmp = atoi (optarg_tmp);
-        printf("F option and argument is: %d \n", tmp );
-#ifdef LPC
-        ret_val=ioctl(file_desc, function, tmp);
-        if (ret_val < 0) {
-            printf("ioctl_set_function failed:%d\n", ret_val);
-            exit(-1);
-        }
-#endif
-        break;
-    }
+
     case 's':{
+        /*
+         * STRING
+         * TODO
+         */
         index_tmp = optind-1;
         while(index_tmp < argc){
             next = argv[index_tmp];
@@ -95,6 +87,10 @@ int main( int argc, char **argv){
         break;
     }
     case 'p':{
+        /*
+         * PICTURE
+         * done
+         */
         tmp = atoi(optarg_tmp);
         printf("P option selected\n");
 #ifdef LPC
@@ -102,11 +98,15 @@ int main( int argc, char **argv){
         if (ret_val < 0) {
             printf("ioctl_set_picture failed:%d\n", ret_val);
             exit(-1);
-        }
+        };
 #endif
         break;
     }
     case 'h':{
+        /*
+         * HELP
+         * done
+         */
         printf("LCD display help: \n");
         printf("\t-f [option]\t\t\t: functions 1 - 4\n");
         printf("\t-s [sentence]\t\t\t: full sentence : print sentence\n");
@@ -115,11 +115,13 @@ int main( int argc, char **argv){
         printf("\t-l [x,y,x1,y1]\t\t\t: plot line\n");
         printf("\t-k [x,y,x1,y1,x2,y2]\t\t: plot square\n");
         printf("\t-c [x,y,x1,y1,x2,y2,collor]\t: fill square with collor\n");
+        printf("\t-c [x,y,r]\t\t\t: plot circle\n\n");
         break;
     }
     case 'l':{
         /*
          * PLOT LINE
+         * done
          */
         index_tmp = optind-1;
         while(index_tmp < argc){
@@ -133,17 +135,44 @@ int main( int argc, char **argv){
             else break;
         }
         optind = index_tmp - 1;
-
         printf("corditantes are : %d %d %d %d ", login_int[0],login_int[1],login_int[2],login_int[3]);
 #ifdef LPC
-        ret_val=ioctl(file_desc, picture, _myPic);
-        if (ret_val < 0) {
-            printf("ioctl_set_picture failed:%d\n", ret_val);
+        ret_val = ioctl(file_desc, linija, login_int);//ovde prosledjujemo sliku kao niz podataka,definisanu u myPic.h
+        if (ret_val < 0){
+            printf("ioctl_set_linija failed:%d\n", ret_val);
             exit(-1);
         }
 #endif
         break;
+    }
+    case 'c':{
+        /*
+         * PLOT CIRCLE
+         * done
+         */
+        printf("crtanje kruga\n");
+        index_tmp = optind-1;
+        while(index_tmp < argc){
+            next = argv[index_tmp];
+            index_tmp++;
+            if(next[0] != '-'){
+                if (index_count < 3) {
+                    login_int[index_count++] = atoi(next);
+                }
+            }
+            else break;
+        }
+        optind = index_tmp - 1;
+        printf("arguments are: %d %d %d /n", login_int[0], login_int[1], login_int[2]);
+#ifdef LPC
+        ret_val = ioctl(file_desc, krug, login_int);
+        if (ret_val < 0){
+                printf("ioctl_set_string failed:%d\n", 0);
+                exit(-1);
+        };
+#endif
 
+        break;
     }
     case -1:{
         break;
@@ -152,8 +181,10 @@ int main( int argc, char **argv){
         abort();
         break;
     }
-
+// ######################################################################################3
     //} while (next_option != -1);
+
+    /*
     if(next_option == 's'){
         for (i=0; i< index_count; i++){
             if( i != index_count-1){
@@ -180,9 +211,19 @@ int main( int argc, char **argv){
             }
         }
     }
-
+*/
     close(file_desc);
 
     return 0;
 }
 
+
+/* TREBA NAM
+ * jos nesto mozda ?
+ *
+ * krug ok
+ * linija ok
+ * string TODO
+ * slika ok
+ *
+ */
